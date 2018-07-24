636d7d7eb64bfc5ecd0f707825fb34793b9093c3
joemitchelljones <joemitchelljones@gmail.com>
2018-07-24 22:28:14 +0100

    @desu -> @here

--- pokerpoll/pokerpoll.py
+++ pokerpoll/pokerpoll.py
@@ -18,7 +18,7 @@ class PokerPoll:
             ]
 
         #Your code will go here
-        poll = await self.bot.say("@desu poker time?\n\ni play to win kid\n--------------------------\n\npoker's for nerds\n--------------------------")
+        poll = await self.bot.say("@here poker time?\n\ni play to win kid\n--------------------------\n\npoker's for nerds\n--------------------------")
 
         for reaction in reactions:
             await self.bot.add_reaction(poll, reaction)
@@ -44,7 +44,7 @@ class PokerPoll:
             good_boys = "\n".join(boy.mention for boy, status in react_hist.items() if status == True)
             bad_boys = "\n".join(boy.mention for boy, status in react_hist.items() if status == False)
             dash = "\n" + "-"*30 + "\n"
-            edit_string = f"@desu poker time?\n\ni play to win kid{dash}{good_boys}\n\npoker's for nerds{dash}{bad_boys}"
+            edit_string = f"@here poker time?\n\ni play to win kid{dash}{good_boys}\n\npoker's for nerds{dash}{bad_boys}"
 
             await self.bot.edit_message(poll, new_content=edit_string)
             
